04349bd019b5587efdfb714e359c1d6afc5ef6c2
Lsmiller17 <gsh686@mocs.utc.edu>
2025-02-27 20:46:18 -0600

    Update Housing_Predictor_App.py

--- Housing_Predictor_App.py
+++ Housing_Predictor_App.py
@@ -6,8 +6,8 @@ import joblib
 import requests
 
 # URL of the model file in GitHub
-MODEL_URL = "https://raw.githubusercontent.com/Lsmiller17/Housing-Price-Predictor/main/Housing_Price_Model_Compatible.pkl"
-MODEL_PATH = "Housing_Price_Model_Compatible.pkl"
+MODEL_URL = "https://raw.githubusercontent.com/Lsmiller17/Housing-Price-Predictor/main/Housing_Price_Model_Updated.pkl"
+MODEL_PATH = "Housing_Price_Model_Updated.pkl"
 
 # Function to download the model if it's missing
 def download_model():
